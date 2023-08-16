232fc60995d27a63da8a2c832d3aaaf6082eaa46
thiago-arend <thiago.arend@gmail.com>
2023-08-15 22:56:01 -0300

    teste: teste verificação funcionamento da branch main

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -4,5 +4,6 @@ import { signout } from "../controllers/auth.controller.js";
 
 const authRouter = Router();
 authRouter.post("/signout", validateAuth, signout);
+// teste
 
 export default authRouter;
