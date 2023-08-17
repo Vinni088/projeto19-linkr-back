b2beb4947505560a7b05702c3275a490d3fdad02
LordAkirast <darkwarish@gmail.com>
2023-08-16 21:50:07 -0300

    feat(timeline-posts-view): Implementa rota de timeline para exibir os 20 posts mais recentes. Criação da rota "/timeline" exibindo 20 posts em ordem decrescente de ID, verificando token de autenticação. Changes: Criação da rota "/timeline" exibindo os 20 posts mais recentes. Ordenação por "id" em ordem decrescente e limite de 20 registros. Verificação do token no cabeçalho. Resposta com posts ou mensagem de ausência. Tratamento de erros e mensagens em falhas.

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import express  from "express"
+import express from "express"
 import cors from "cors"
 import router from "./routes/index.routes.js"
 
@@ -7,6 +7,40 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(router)
+
+
+
+app.get("/timeline", async (req, res) => {
+
+    ///token pode vir pelo header
+    if (!token) {
+        console.log("UNAUTHORIZED! TOKEN IS MISSING!")
+        return res.status(409).send("UNAUTHORIZED! TOKEN IS MISSING!")
+    }
+
+
+    try {
+
+        //ordena todos os posts do id mais antigo para o mais atual
+        const posts = await db.query('SELECT * FROM POST ORDER BY id DESC LIMIT 20;')
+
+        console.log("GET POSTS SUCCESS")
+
+        if (posts.rows.length < 1) {
+
+            const postData = {message: 'There are no posts yet'}
+
+            return res.status(200).send(postData)
+
+        } else {
+            return res.status(200).send(posts.rows)
+        }
+    } catch (error) {
+        console.log(error.message)
+        return res.status(500).send(error.message)
+    }
+
+})
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Running server on port ${PORT}`));
