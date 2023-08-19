dc48c7f09d20b1d576ee87efb65672086e0eaf00
thiago-arend <thiago.arend@gmail.com>
2023-08-19 01:52:45 -0300

    fix: return-posts-related-to-hashtag

--- src/repositories/posts.repository.js
+++ src/repositories/posts.repository.js
@@ -3,7 +3,7 @@ import { db } from "../database/database.connection.js";
 export function returnPostsRelatedToHashtag(hashtag) {
     return db.query(`
         SELECT p.id, p.url, p.description, u.username, u."photoUrl", 
-        (SELECT COUNT (*) FROM "like" WHERE "postId"=p.id ORDER BY "createdAt" DESC) AS "likeCount",
+        (SELECT COUNT (*) FROM "like" WHERE "postId"=p.id) AS "likeCount",
         (SELECT JSON_AGG(u.username) FROM "like" l JOIN "user" u ON u.id=l."userId" JOIN post p ON p.id=l."postId" WHERE p.id=ph."postId") AS "whoLikedList"
             FROM "postHasHashtag" ph
                 JOIN post p ON p.id=ph."postId"
