3116865286f5f5c718de9ca2aab962e1c0e9a7cb
Vinicius Evangelista <viniciusevangelista088@gmail.comgit config --global user.name Vinni>
2023-08-19 23:36:06 -0300

    feat: get timeline online

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -82,3 +82,46 @@ export async function savePost(req, res) {
         return res.status(500).send(err.message);
     }
 }
+
+export async function getPostsTimeline(req, res) {
+    const { id } = req.params; //id do usuario do qual queremos os posts
+    const UserId = res.locals.session.id; // id do usuario que está vendo os posts
+
+    try {
+
+        const posts = (await db.query(`
+        SELECT 
+            post.id, (SELECT "user".username FROM "user" WHERE "user".id = post."userId"),
+            post.url, post.description,
+            CAST(COUNT("like".*) AS INTEGER) AS "numberOfLikes"
+        FROM post
+        LEFT JOIN "like"
+            ON "like"."postId" = post.id
+        LEFT JOIN "user"
+            ON "user".id = "post"."userId"
+        GROUP BY post.id
+        `)).rows
+
+        let likedBy = (await db.query(`
+        SELECT * FROM "like"
+        WHERE "userId" = $1
+        `, [UserId])).rows
+
+        likedBy = likedBy.map(like => like.postId)
+        let resposta = posts.map(post => {
+            return {
+                userId: post.userId,
+                postId: post.id,
+                postUrl: post.url,
+                postOwner: post.username,
+                postDescription: post.description,
+                numberOfLikes: post.numberOfLikes,
+                likedByViewer: (likedBy.includes(post.id) ? true : false)
+            }
+        })
+
+        return res.status(200).send(resposta)
+    } catch (error) {
+        return res.status(500).send(error.message)
+    }
+};
--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express"
 import { validateAuth } from "../middlewares/validateAuth.js";
-import { getPostsByUser, createToken, savePost } from "../controllers/post.controller.js";
+import { getPostsByUser, getPostsTimeline, createToken, savePost } from "../controllers/post.controller.js";
 
 
 const postsRouter = Router();
 
+postsRouter.get("/posts/timeline", validateAuth, getPostsTimeline)
 postsRouter.get("/posts/user/:id", validateAuth, getPostsByUser);
 postsRouter.get("/createToken", createToken)
 postsRouter.post("/timeline", validateAuth, savePost)
