f055e64e13592e72d412f497e0842620ff427157
Matheus <matheusvmja@gmail.com>
2023-08-19 21:59:00 -0300

    Feat: Posts saved on the database

--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express"
 import { validateAuth } from "../middlewares/validateAuth.js";
-import { getPostsByUser, createToken } from "../controllers/post.controller.js";
+import { getPostsByUser, createToken, savePost } from "../controllers/post.controller.js";
 
 
 const postsRouter = Router();
 
 postsRouter.get("/posts/user/:id", validateAuth, getPostsByUser);
 postsRouter.get("/createToken", createToken)
+postsRouter.post("/timeline", validateAuth, savePost)
 
 export default postsRouter;
