fdd238db505b467f91beca5ea4541c85dcce00f2
Matheus <matheusvmja@gmail.com>
2023-08-20 18:09:32 -0300

    Feat: getUsersBySearch finished

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express"
-import { getUsers, signUp } from "../controllers/user.controller.js"
+import { getUsers, getUsersBySearch, signUp } from "../controllers/user.controller.js"
 import { validateSchema } from "../middlewares/validateSchema.js"
 import { signUpSchema } from "../schemas/users.schemas.js"
 import { validateAuth } from "../middlewares/validateAuth.js"
@@ -8,5 +8,6 @@ const usersRouter = Router()
 
 usersRouter.post("/signup", validateSchema(signUpSchema), signUp)
 usersRouter.get("/autenticated-user", validateAuth, getUsers);
+usersRouter.get("/search-users", getUsersBySearch)
 
 export default usersRouter
