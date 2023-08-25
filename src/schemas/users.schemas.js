3a5da61a6b38234f12339d5759e07f5b48cb9306
Vinicius Evangelista <viniciusevangelista088@gmail.comgit config --global user.name Vinni>
2023-08-25 17:21:42 -0300

    fix: scheema signup

--- src/schemas/users.schemas.js
+++ src/schemas/users.schemas.js
@@ -5,7 +5,7 @@ export const signUpSchema = joi.object({
     username: joi.string().required(),
     email: joi.string().email().required(),
     password: joi.string().required(),
-    photoUrl: joi.string().uri().required()
+    photoUrl: joi.string().required()
 });
 
 // export const signUpSchema = joi.object({
